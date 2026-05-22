52980bc85f071957ef08262ffce1173e6fd7b139
VrezenStrijder <f94fbr@outlook.com>
2026-05-22 18:41:39 +0800

    Create DensityMode.cs

---
+++ SmartWPF.Core/Appearance/DensityMode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartWPF.Core.Appearance;
+
+/// <summary>
+/// 视觉密度模式
+/// </summary>
+public enum DensityMode
+{
+    Compact,        // 紧凑: 小字体、窄间距, 适合数据密集型/工具型应用
+    Default,        // 默认: 标准桌面应用体验
+    Presentation    // 演示: 大字体、宽间距, 适合演示/触屏/无障碍
+}
