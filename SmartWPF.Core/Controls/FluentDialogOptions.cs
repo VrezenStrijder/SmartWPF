2d8b57bd77797d2955ec56b57f68f3c28f4a98f3
VrezenStrijder <f94fbr@outlook.com>
2026-06-05 18:20:00 +0800

    Create FluentDialogOptions.cs

---
+++ SmartWPF.Core/Controls/FluentDialogOptions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartWPF.Core.Controls;
+
+public enum FluentDialogType
+{
+    Information,
+    Success,
+    Warning,
+    Error,
+    Confirm
+}
+
+public enum FluentDialogButton
+{
+    OK,
+    OKCancel,
+    YesNo,
+    YesNoCancel
+}
+
+public class FluentDialogOptions
+{
+    public string Title { get; init; } = "";
+    public string Message { get; init; } = "";
+    public FluentDialogType Type { get; init; } = FluentDialogType.Information;
+    public FluentDialogButton Buttons { get; init; } = FluentDialogButton.OK;
+    public string? PrimaryButtonText { get; init; }
+    public string? SecondaryButtonText { get; init; }
+    public string? CancelButtonText { get; init; }
+
+    /// <summary>
+    /// 是否允许拖动对话框
+    /// </summary>
+    public bool AllowDrag { get; init; } = true;
+}
+
