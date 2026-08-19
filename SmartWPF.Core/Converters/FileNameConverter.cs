4347f091275cc8703cad888b8892821823ed11b1
VrezenStrijder <f94fbr@outlook.com>
2026-08-19 18:12:50 +0800

    Create FileNameConverter.cs

---
+++ SmartWPF.Core/Converters/FileNameConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartWPF.Core.Converters;
+
+public class FileNameConverter : ValueConverterBase
+{
+    /// <summary>
+    /// 是否包含扩展名 
+    /// </summary>
+    public bool IncludeExtension { get; set; } = true;
+
+    public override object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not string path || string.IsNullOrEmpty(path))
+        {
+            return "";
+        }
+
+        return IncludeExtension ? Path.GetFileName(path) : Path.GetFileNameWithoutExtension(path);
+    }
+}
